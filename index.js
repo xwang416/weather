706f0b74c2a9f7473852605d415e898adbf0885f
xwang416 <xwang416@gmail.com>
2017-04-02 21:22:45 -0400

    show result on the web page (one city query only)

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ var weatherMap = require('./weatherMap.js');
 var http = require('http');
 var port = 9000;
 
-var requestHandler = (request, response) => {  
+var requestHandler = (request, response) => {
     var url = request.url;
-    
+
     //avoid anoying favicon request from browser
-    if(url === '/favicon.ico'){
-        response.writeHead(200, {'Content-Type':'image/x-icon'});
+    if (url === '/favicon.ico') {
+        response.writeHead(200, { 'Content-Type': 'image/x-icon' });
         response.end();
         return;
     }
-    
-    var arr = url.split('/');
-    var cities = arr[arr.length-1].split('%7C');
-    
-    cities.forEach(function(city){
 
-        weatherMap.getWeather(city);
+    var arr = url.split('/');
+    var cities = arr[arr.length - 1].split('%7C');
+
+    cities.forEach(function(city) {
+
+        weatherMap.getWeather(city, response);
 
     });
-  
-    response.end('Results are shown in console.');
+
+
+
+    //response.end('Results are shown in console.\n');
 };
 
 var server = http.createServer(requestHandler);
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
--- weatherMap.js
+++ weatherMap.js
@@ -1,48 +1,52 @@
 const http = require('http');
-const db = require('./db.js');
+//const db = require('./db.js');
 
 module.exports = {
-    
-    getWeather: function(city){
-        
+
+    getWeather: function(city, serverRes) {
+
         var result;
 
         var options = {
             host: 'api.openweathermap.org',
             port: '80',
-            path: '/data/2.5/weather?q='+city+'&APPID=8b4cfba64690a44aa14a0504e21d7129',
+            path: '/data/2.5/weather?q=' + city + '&APPID=8b4cfba64690a44aa14a0504e21d7129',
             method: 'GET'
         };
 
-        http.request(options, function(res){
-            
+        http.request(options, function(res) {
+
             var body = '';
             const absZero = 273.15;
-            
-            res.setEncoding('utf8');
-            res.on('error', function(err){
-                console.log('There is an error', err);
-            })
-            .on('data', function (data) {
-                body += data;
-            })
-            .on('end', function () {
-                result = JSON.parse(body);
-                
-                var json = {
-                    city: city,
-                    statusCode: res.statusCode,
-                    temp: Math.round(result.main.temp - absZero)
-                }
-                
-                console.log('\n' + json.city);
-                console.log('http status: ' + json.statusCode) 
-                console.log('current temp: '+ json.temp +'C\n------------------\n');
-                
-                db.insertCityWeather(json);
 
-            })
+            res.setEncoding('utf8');
+            res.on('error', function(err) {
+                    console.log('There is an error', err);
+                })
+                .on('data', function(data) {
+                    body += data;
+                })
+                .on('end', function() {
+                    result = JSON.parse(body);
+
+                    var json = {
+                        city: city,
+                        statusCode: res.statusCode,
+                        temp: Math.ceil(result.main.temp - absZero)
+                    }
+
+                    console.log('\n' + json.city);
+                    console.log('http status: ' + json.statusCode)
+                    console.log('current temp: ' + json.temp + 'C\n------------------\n');
+
+                    //db.insertCityWeather(json);
+                    serverRes.end(JSON.stringify(json));
+
+                });
+
 
         }).end();
+
+        //setTimeout(function() { console.log(result); }, 500);
     }
 };
